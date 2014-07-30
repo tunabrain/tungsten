cd8efed048052810390652ef7149eaeb43d1a4dd
Benedikt Bitterli <mail@noobody.org>
2014-07-30 19:00:23 +0200

    Removed potential hindrance to stratification

--- src/core/sampling/SampleGenerator.hpp
+++ src/core/sampling/SampleGenerator.hpp
@@ -7,7 +7,6 @@
 #include "math/BitManip.hpp"
 #include "math/MathUtil.hpp"
 
-#include "Config.hpp"
 #include "Debug.hpp"
 
 namespace Tungsten
@@ -18,7 +17,7 @@ class SampleGenerator
 public:
     virtual ~SampleGenerator() {}
 
-    virtual void setup(uint32 pixel, int sample) = 0;
+    virtual void setup(uint32 pixelId, int sample) = 0;
 
     virtual uint32 nextI() = 0;
 
@@ -29,17 +28,26 @@ public:
 
     virtual Vec2f next2D()
     {
-        return Vec2f(next1D(), next1D());
+        float a = next1D();
+        float b = next1D();
+        return Vec2f(a, b);
     }
 
     virtual Vec3f next3D()
     {
-        return Vec3f(next1D(), next1D(), next1D());
+        float a = next1D();
+        float b = next1D();
+        float c = next1D();
+        return Vec3f(a, b, c);
     }
 
     virtual Vec4f next4D()
     {
-        return Vec4f(next1D(), next1D(), next1D(), next1D());
+        float a = next1D();
+        float b = next1D();
+        float c = next1D();
+        float d = next1D();
+        return Vec4f(a, b, c, d);
     }
 };
 
@@ -48,15 +56,6 @@ class SobolSampler : public SampleGenerator
     uint32 _scramble;
     uint32 _index;
     int _dimension;
-
-    inline uint32 invert4Bit(uint32 x) const
-    {
-        return
-            ((x & 1) << 3) |
-            ((x & 2) << 1) |
-            ((x & 4) >> 1) |
-            ((x & 8) >> 3);
-    }
 
     inline uint32 permutedIndex() const
     {
@@ -69,9 +68,9 @@ public:
     {
     }
 
-    void setup(uint32 pixel, int sample) override final
+    void setup(uint32 pixelId, int sample) override final
     {
-        _scramble = MathUtil::hash32(pixel);
+        _scramble = MathUtil::hash32(pixelId);
         _index = sample;
         _dimension = 0;
     }
@@ -80,42 +79,10 @@ public:
     {
         if (_dimension >= 1024)
             FAIL("Sobol sampler dimension > 1024!");
-        return sobol::sample(permutedIndex(), _dimension++, _scramble);// + _scramble;
+        return sobol::sample(permutedIndex(), _dimension++, _scramble);
     }
 };
 
-#if 0
-class UniformSampler : public SampleGenerator
-{
-    uint32 _seed;
-
-public:
-    UniformSampler()
-    : _seed(0xBA5EBA11)
-    {
-    }
-
-    UniformSampler(uint32 seed)
-    : _seed(seed)
-    {
-    }
-
-    void setup(uint32 /*pixel*/, int /*sample*/) override final
-    {
-    }
-
-    uint32 nextI() override final
-    {
-        _seed = (_seed*1103515245u + 12345u) & 0x7FFFFFFF;
-        return _seed;
-    }
-
-    virtual float next1D()
-    {
-        return BitManip::normalizedUint(nextI() << 1);
-    }
-};
-#else
 class UniformSampler : public SampleGenerator
 {
     SFMT_T _state;
@@ -131,7 +98,7 @@ public:
         sfmt_init_gen_rand(&_state, seed);
     }
 
-    void setup(uint32 /*pixel*/, int /*sample*/) override final
+    void setup(uint32 /*pixelId*/, int /*sample*/) override final
     {
     }
 
@@ -145,7 +112,6 @@ public:
         return BitManip::normalizedUint(nextI());
     }
 };
-#endif
 
 }
 
