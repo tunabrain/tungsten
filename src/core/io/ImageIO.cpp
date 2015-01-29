031d06e6f0fd18e26991c4f036695b3193096cd1
Benedikt Bitterli <mail@noobody.org>
2015-01-29 20:44:59 +0100

    Added gamma correction to textures

--- src/core/io/ImageIO.cpp
+++ src/core/io/ImageIO.cpp
@@ -6,6 +6,25 @@
 #include <stbi/stb_image.h>
 #include <cstring>
 
+static const int GammaCorrection[] = {
+      0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   1,   1,   1,   1,   1,   1,   1,   1,   2,   2,   2,
+      2,   2,   3,   3,   3,   3,   3,   4,   4,   4,   4,   5,   5,   5,   5,   6,
+      6,   6,   7,   7,   7,   8,   8,   8,   9,   9,   9,  10,  10,  10,  11,  11,
+     12,  12,  13,  13,  13,  14,  14,  15,  15,  16,  16,  17,  17,  18,  18,  19,
+     19,  20,  21,  21,  22,  22,  23,  23,  24,  25,  25,  26,  27,  27,  28,  29,
+     29,  30,  31,  31,  32,  33,  33,  34,  35,  36,  36,  37,  38,  39,  40,  40,
+     41,  42,  43,  44,  45,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,
+     55,  56,  57,  58,  59,  60,  61,  62,  63,  65,  66,  67,  68,  69,  70,  71,
+     72,  73,  74,  75,  77,  78,  79,  80,  81,  82,  84,  85,  86,  87,  88,  90,
+     91,  92,  93,  95,  96,  97,  99, 100, 101, 103, 104, 105, 107, 108, 109, 111,
+    112, 114, 115, 117, 118, 119, 121, 122, 124, 125, 127, 128, 130, 131, 133, 135,
+    136, 138, 139, 141, 142, 144, 146, 147, 149, 151, 152, 154, 156, 157, 159, 161,
+    162, 164, 166, 168, 169, 171, 173, 175, 176, 178, 180, 182, 184, 186, 187, 189,
+    191, 193, 195, 197, 199, 201, 203, 205, 207, 209, 211, 213, 215, 217, 219, 221,
+    223, 225, 227, 229, 231, 233, 235, 237, 239, 241, 244, 246, 248, 250, 252, 255
+};
+
 namespace Tungsten {
 
 namespace ImageIO {
@@ -127,7 +146,7 @@ std::unique_ptr<uint8[], void(*)(void *)> loadStbi(const std::string &file,
     return std::unique_ptr<uint8[], void(*)(void *)>(stbi_load(file.c_str(), &w, &h, &channels, 4), stbi_image_free);
 }
 
-std::unique_ptr<uint8[]> loadLdr(const std::string &file, TexelConversion request, int &w, int &h)
+std::unique_ptr<uint8[]> loadLdr(const std::string &file, TexelConversion request, int &w, int &h, bool gammaCorrect)
 {
     int channels;
     std::unique_ptr<uint8[], void(*)(void *)> img(nullptr, free);
@@ -144,6 +163,11 @@ std::unique_ptr<uint8[]> loadLdr(const std::string &file, TexelConversion reques
     std::unique_ptr<uint8[]> texels(new uint8[w*h*targetChannels]);
     if (targetChannels == 4) {
         std::memcpy(texels.get(), img.get(), w*h*targetChannels*sizeof(uint8));
+
+        if (gammaCorrect)
+            for (int i = 0; i < w*h; ++i)
+                for (int t = 0; t < 3; ++t)
+                    texels[i*4 + t] = GammaCorrection[texels[i*4 + t]];
     } else {
         for (int i = 0; i < w*h; ++i)
             texels[i] = convertToScalar(request, int(img[i*4]), int(img[i*4 + 1]), int(img[i*4 + 2]),
--- src/core/io/ImageIO.hpp
+++ src/core/io/ImageIO.hpp
@@ -24,7 +24,8 @@ namespace ImageIO {
 bool isHdr(const std::string &file);
 
 std::unique_ptr<float[]> loadHdr(const std::string &file, TexelConversion request, int &w, int &h);
-std::unique_ptr<uint8[]> loadLdr(const std::string &file, TexelConversion request, int &w, int &h);
+std::unique_ptr<uint8[]> loadLdr(const std::string &file, TexelConversion request, int &w, int &h,
+        bool gammaCorrect = true);
 
 bool saveHdr(const std::string &file, const float *img, int w, int h, int channels);
 bool saveLdr(const std::string &file, const uint8 *img, int w, int h, int channels);
