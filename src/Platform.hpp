d0768ee9c6ecd95954c887f2e48c46e948fd1299
Benedikt Bitterli <mail@noobody.org>
2015-02-08 20:01:42 +0100

    Added FORCE_INLINE makro

---
+++ src/Platform.hpp
@@ -0,0 +1,12 @@
+#ifndef PLATFORM_HPP_
+#define PLATFORM_HPP_
+
+#if defined(_MSC_VER)
+#define FORCE_INLINE __forceinline
+#elif defined(__GNUC__)
+#define FORCE_INLINE inline __attribute__((always_inline))
+#else
+#define FORCE_INLINE
+#endif
+
+#endif /* PLATFORM_HPP_ */
