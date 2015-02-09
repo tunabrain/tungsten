d868fa00a756f1cf8ae574e16446d7b51c291fb4
Benedikt Bitterli <mail@noobody.org>
2015-02-09 01:13:41 +0100

    Added debug output and Model 63 to auto optimization flag module

--- cmake/OptimizeForArchitecture.cmake
+++ cmake/OptimizeForArchitecture.cmake
@@ -68,11 +68,14 @@ macro(AutodetectHostArchitecture)
       string(REGEX REPLACE ".* Family ([0-9]+) .*" "\\1" _cpu_family "${_cpu_id}")
       string(REGEX REPLACE ".* Model ([0-9]+) .*" "\\1" _cpu_model "${_cpu_id}")
    endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+   message(STATUS "Detected: Vendor ID '${_vendor_id}'   CPU Family '${_cpu_family}'  CPU Model '${_cpu_model}'  CPU Flags  '${_cpu_flags}'")
    if(_vendor_id STREQUAL "GenuineIntel")
       if(_cpu_family EQUAL 6)
          # Any recent Intel CPU except NetBurst
          if(_cpu_model EQUAL 69)    # Core i5/i7-4xxxU CPUs
             set(TARGET_ARCHITECTURE "haswell")
+         elseif(_cpu_model EQUAL 63) # Xeon E5 series
+            set(TARGET_ARCHITECTURE "sandy-bridge")
          elseif(_cpu_model EQUAL 62)
             set(TARGET_ARCHITECTURE "ivy-bridge")
          elseif(_cpu_model EQUAL 60)
