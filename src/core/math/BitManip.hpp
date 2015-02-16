ba50d690ce9f30ef2ab56a6af826b793e56a9368
Benedikt Bitterli <mail@noobody.org>
2015-02-17 00:06:01 +0100

    Implemented simple string hash function for hash values portable across compilers

--- src/core/math/BitManip.hpp
+++ src/core/math/BitManip.hpp
@@ -3,6 +3,8 @@
 
 #include "IntTypes.hpp"
 
+#include <string>
+
 namespace Tungsten {
 
 class BitManip
@@ -35,6 +37,18 @@ public:
     static inline float normalizedUint(uint32 i)
     {
         return uintBitsToFloat((i >> 9u) | 0x3F800000u) - 1.0f;
+    }
+
+    // std::hash is not necessarily portable across compilers.
+    // In case a consistent hash function is needed (for file IO),
+    // this will do. This function is what is used in gawk.
+    // See also http://www.cse.yorku.ca/~oz/hash.html
+    static inline uint64 hash(const std::string &s)
+    {
+        uint64 result = 0;
+        for (char c : s)
+            result = (result*65599ull) + uint64(c);
+        return result;
     }
 };
 
