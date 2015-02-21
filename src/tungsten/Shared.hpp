ab772b9123a17682acf1b9dc1908165fa87cc5cc
Benedikt Bitterli <mail@noobody.org>
2015-02-21 01:15:45 +0100

    Move standalone renderer functionality into shared header file

---
+++ src/tungsten/Shared.hpp
@@ -0,0 +1,324 @@
+#ifndef SHARED_HPP_
+#define SHARED_HPP_
+
+#include "renderer/TraceableScene.hpp"
+#include "renderer/Renderer.hpp"
+
+#include "thread/ThreadUtils.hpp"
+
+#include "io/DirectoryChange.hpp"
+#include "io/FileUtils.hpp"
+#include "io/JsonUtils.hpp"
+#include "io/CliParser.hpp"
+#include "io/Scene.hpp"
+
+#include "Timer.hpp"
+
+#include <tinyformat/tinyformat.hpp>
+#include <embree/include/embree.h>
+#include <rapidjson/document.h>
+#include <cstdlib>
+#include <vector>
+#include <deque>
+
+namespace Tungsten {
+
+static std::string formatTime(double elapsed)
+{
+    uint64 seconds = uint64(elapsed);
+    uint64 minutes = seconds/60;
+    uint64 hours = minutes/60;
+    uint64 days = hours/24;
+    double fraction = elapsed - seconds;
+
+    std::stringstream ss;
+
+    if (days)    ss << tfm::format("%dd ", days);
+    if (hours)   ss << tfm::format("%dh ", hours % 24);
+    if (minutes) ss << tfm::format("%dm ", minutes % 60);
+    if (seconds) ss << tfm::format("%ds %dms", seconds % 60, uint64(fraction*1000.0f) % 1000);
+    else ss << elapsed << "s";
+
+    return ss.str();
+}
+
+static const int OPT_CHECKPOINTS       = 0;
+static const int OPT_THREADS           = 1;
+static const int OPT_VERSION           = 2;
+static const int OPT_HELP              = 3;
+static const int OPT_RESTART           = 4;
+static const int OPT_OUTPUT_DIRECTORY  = 5;
+
+enum RenderState
+{
+    STATE_LOADING,
+    STATE_RENDERING,
+};
+
+static const char *renderStateToString(RenderState state)
+{
+    switch (state) {
+    case STATE_LOADING:   return "loading";
+    case STATE_RENDERING: return "rendering";
+    default:              return "unknown";
+    }
+}
+
+struct RendererStatus
+{
+    RenderState state;
+    int currentSpp;
+    int nextSpp;
+    int totalSpp;
+
+    std::vector<Path> completedScenes;
+    Path currentScene;
+    std::deque<Path> queuedScenes;
+
+    rapidjson::Value toJson(rapidjson::Document::AllocatorType &allocator) const
+    {
+        rapidjson::Value result(rapidjson::kObjectType);
+        result.SetObject();
+
+        result.AddMember("state", renderStateToString(state), allocator);
+        result.AddMember("current_spp", currentSpp, allocator);
+        result.AddMember("next_spp", nextSpp, allocator);
+        result.AddMember("total_spp", totalSpp, allocator);
+        result.AddMember("current_scene", currentScene.asString().c_str(), allocator);
+        if (!completedScenes.empty()) {
+            rapidjson::Value v(rapidjson::kArrayType);
+            for (const Path &p : completedScenes)
+                v.PushBack(p.asString().c_str(), allocator);
+            result.AddMember("completed_scenes", std::move(v), allocator);
+        }
+        if (!queuedScenes.empty()) {
+            rapidjson::Value v(rapidjson::kArrayType);
+            for (const Path &p : queuedScenes)
+                v.PushBack(p.asString().c_str(), allocator);
+            result.AddMember("queued_scenes", std::move(v), allocator);
+        }
+
+        return std::move(result);
+    }
+};
+
+class StandaloneRenderer
+{
+    CliParser &_parser;
+    std::ostream &_logStream;
+
+    int _checkpointInterval;
+    int _threadCount;
+    Path _outputDirectory;
+
+    std::unique_ptr<Scene> _scene;
+    std::unique_ptr<TraceableScene> _flattenedScene;
+    std::unique_ptr<Renderer> _renderer;
+
+    std::mutex _statusMutex;
+    std::mutex _logMutex;
+    std::mutex _sceneMutex;
+    RendererStatus _status;
+
+    void writeLogLine(const std::string &s)
+    {
+        std::unique_lock<std::mutex> lock(_logMutex);
+        _logStream << s << std::endl;
+    }
+
+public:
+    StandaloneRenderer(CliParser &parser, std::ostream &logStream)
+    : _parser(parser),
+      _logStream(logStream),
+      _checkpointInterval(0),
+      _threadCount(max(ThreadUtils::idealThreadCount() - 1, 1u))
+    {
+        _status.state = STATE_LOADING;
+        _status.currentSpp = _status.nextSpp = _status.totalSpp = 0;
+
+        parser.addOption('h', "help", "Prints this help text", false, OPT_HELP);
+        parser.addOption('v', "version", "Prints version information", false, OPT_VERSION);
+        parser.addOption('t', "threads", "Specifies number of threads to use (default: number of cores minus one)", true, OPT_THREADS);
+        parser.addOption('r', "restart", "Ignores saved render checkpoints and starts fresh from 0 spp", false, OPT_RESTART);
+        parser.addOption('c', "checkpoint", "Specifies render time in minutes before saving a checkpoint. A value of 0 disables checkpoints. Overrides the setting in the scene file", true, OPT_CHECKPOINTS);
+        parser.addOption('o', "output-directory", "Specifies the output directory. Overrides the setting in the scene file", true, OPT_OUTPUT_DIRECTORY);
+    }
+
+    void setup()
+    {
+        if (_parser.operands().empty() || _parser.isPresent(OPT_HELP)) {
+            _parser.printHelpText();
+            std::exit(0);
+        }
+
+        if (_parser.isPresent(OPT_THREADS)) {
+            int newThreadCount = std::atoi(_parser.param(OPT_THREADS).c_str());
+            if (newThreadCount > 0)
+                _threadCount = newThreadCount;
+        }
+        if (_parser.isPresent(OPT_CHECKPOINTS))
+            _checkpointInterval = std::atoi(_parser.param(OPT_CHECKPOINTS).c_str());
+
+        embree::rtcInit();
+        embree::rtcStartThreads(_threadCount);
+
+        ThreadUtils::startThreads(_threadCount);
+
+        if (_parser.isPresent(OPT_OUTPUT_DIRECTORY)) {
+            _outputDirectory = Path(_parser.param(OPT_OUTPUT_DIRECTORY));
+            _outputDirectory.freezeWorkingDirectory();
+            _outputDirectory = _outputDirectory.absolute();
+        }
+
+        for (const std::string &p : _parser.operands())
+            _status.queuedScenes.emplace_back(p);
+    }
+
+    bool renderScene()
+    {
+        Path currentScene;
+        {
+            std::unique_lock<std::mutex> lock(_statusMutex);
+            if (_status.queuedScenes.empty())
+                return false;
+
+            _status.state = STATE_LOADING;
+            _status.currentSpp = _status.nextSpp = _status.totalSpp = 0;
+
+            currentScene = _status.currentScene = _status.queuedScenes.front();
+            _status.queuedScenes.pop_front();
+        }
+
+        writeLogLine(tfm::format("Loading scene '%s'...", currentScene));
+        try {
+            std::unique_lock<std::mutex> lock(_sceneMutex);
+            _scene.reset(Scene::load(Path(currentScene)));
+            _scene->loadResources();
+        } catch (std::runtime_error &e) {
+            writeLogLine(tfm::format("Scene loader for file '%s' encountered an unrecoverable error: \n%s",
+                    currentScene, e.what()));
+
+            std::unique_lock<std::mutex> lock(_sceneMutex);
+            _scene.reset();
+
+            return true;
+        }
+
+        {
+            std::unique_lock<std::mutex> lock(_statusMutex);
+            _status.totalSpp = _scene->rendererSettings().spp();
+        }
+
+        try {
+            DirectoryChange context(_scene->path().parent());
+
+            if (_parser.isPresent(OPT_OUTPUT_DIRECTORY))
+                _scene->rendererSettings().setOutputDirectory(_outputDirectory);
+
+            int maxSpp = _scene->rendererSettings().spp();
+            {
+                std::unique_lock<std::mutex> lock(_sceneMutex);
+                _flattenedScene.reset(_scene->makeTraceable());
+                _renderer.reset(new Renderer(*_flattenedScene));
+            }
+
+            if (!_parser.isPresent(OPT_CHECKPOINTS))
+                _checkpointInterval = _scene->rendererSettings().checkpointInterval();
+
+            if (_scene->rendererSettings().enableResumeRender() && !_parser.isPresent(OPT_RESTART)) {
+                writeLogLine("Trying to resume render from saved state... ");
+                if (_renderer->resumeRender(*_scene))
+                    writeLogLine("Resume successful");
+                else
+                    writeLogLine("Resume unsuccessful. Starting from 0 spp");
+            }
+
+            writeLogLine("Starting render...");
+            Timer timer, checkpointTimer;
+            double totalElapsed = 0.0;
+            while (!_renderer->done()) {
+                {
+                    std::unique_lock<std::mutex> lock(_statusMutex);
+                    _status.state = STATE_RENDERING;
+                    _status.currentSpp = _renderer->currentSpp();
+                    _status.nextSpp = _renderer->nextSpp();
+                }
+
+                _renderer->startRender([](){});
+                _renderer->waitForCompletion();
+                writeLogLine(tfm::format("Completed %d/%d spp", _renderer->currentSpp(), maxSpp));
+                checkpointTimer.stop();
+                if (_checkpointInterval > 0 && checkpointTimer.elapsed() > _checkpointInterval*60) {
+                    totalElapsed += checkpointTimer.elapsed();
+                    writeLogLine(tfm::format("Saving checkpoint after %s", formatTime(totalElapsed).c_str()));
+                    Timer ioTimer;
+                    checkpointTimer.start();
+                    _renderer->saveCheckpoint();
+                    if (_scene->rendererSettings().enableResumeRender())
+                        _renderer->saveRenderResumeData(*_scene);
+                    ioTimer.stop();
+                    writeLogLine(tfm::format("Saving checkpoint took %s", formatTime(ioTimer.elapsed())));
+                }
+            }
+            timer.stop();
+
+            writeLogLine(tfm::format("Finished render. Render time %s", formatTime(timer.elapsed()).c_str()));
+
+            _renderer->saveOutputs();
+            if (_scene->rendererSettings().enableResumeRender())
+                _renderer->saveRenderResumeData(*_scene);
+
+            {
+                std::unique_lock<std::mutex> lock(_statusMutex);
+                _status.completedScenes.push_back(currentScene);
+            }
+        } catch (std::runtime_error &e) {
+            writeLogLine(tfm::format("Renderer for file '%s' encountered an unrecoverable error: \n%s",
+                    currentScene, e.what()));
+        }
+
+        {
+            std::unique_lock<std::mutex> lock(_sceneMutex);
+            _renderer.reset();
+            _flattenedScene.reset();
+            _scene.reset();
+        }
+
+        return true;
+    }
+
+    RendererStatus status()
+    {
+        std::unique_lock<std::mutex> lock(_statusMutex);
+        RendererStatus copy(_status);
+        return std::move(copy);
+    }
+
+    std::mutex &logMutex()
+    {
+        return _logMutex;
+    }
+
+    std::unique_ptr<Vec3c[]> frameBuffer(Vec2i &resolution)
+    {
+        std::unique_lock<std::mutex> lock(_sceneMutex);
+        if (!_renderer || !_scene || !_flattenedScene)
+            return nullptr;
+
+        Vec2u res = _scene->camera()->resolution();
+        std::unique_ptr<Vec3c[]> ldr(new Vec3c[res.product()]);
+
+        for (uint32 y = 0; y < res.y(); ++y)
+            for (uint32 x = 0; x < res.x(); ++x)
+                ldr[x + y*res.x()] = Vec3c(clamp(Vec3i(_scene->camera()->get(x, y)*255.0f), Vec3i(0), Vec3i(255)));
+
+        resolution = Vec2i(res);
+
+        return std::move(ldr);
+    }
+};
+
+}
+
+#endif /* SHARED_HPP_ */
+
--- src/tungsten/tungsten.cpp
+++ src/tungsten/tungsten.cpp
@@ -1,164 +1,24 @@
 #include "Version.hpp"
-#include "Timer.hpp"
-
-#include "renderer/TraceableScene.hpp"
-#include "renderer/Renderer.hpp"
-
-#include "cameras/Camera.hpp"
-
-#include "thread/ThreadUtils.hpp"
-
-#include "io/DirectoryChange.hpp"
-#include "io/FileUtils.hpp"
-#include "io/CliParser.hpp"
-#include "io/Scene.hpp"
-
-#include <tinyformat/tinyformat.hpp>
-#include <embree/include/embree.h>
-
-namespace Tungsten {
-
-std::string formatTime(double elapsed)
-{
-    uint64 seconds = uint64(elapsed);
-    uint64 minutes = seconds/60;
-    uint64 hours = minutes/60;
-    uint64 days = hours/24;
-    double fraction = elapsed - seconds;
-
-    std::stringstream ss;
-
-    if (days)    ss << tfm::format("%dd ", days);
-    if (hours)   ss << tfm::format("%dh ", hours % 24);
-    if (minutes) ss << tfm::format("%dm ", minutes % 60);
-    if (seconds) ss << tfm::format("%ds %dms", seconds % 60, uint64(fraction*1000.0f) % 1000);
-    else ss << elapsed << "s";
-
-    return ss.str();
-}
-
-}
+#include "Shared.hpp"
 
 using namespace Tungsten;
 
-static const int OPT_CHECKPOINTS       = 0;
-static const int OPT_THREADS           = 1;
-static const int OPT_VERSION           = 2;
-static const int OPT_HELP              = 3;
-static const int OPT_RESTART           = 4;
-static const int OPT_OUTPUT_DIRECTORY  = 5;
-
 int main(int argc, const char *argv[])
 {
-    int checkpointInterval = 0;
-    int threadCount = max(ThreadUtils::idealThreadCount() - 1, 1u);
-
     CliParser parser("tungsten", "[options] scene1 [scene2 [scene3...]]");
-    parser.addOption('h', "help", "Prints this help text", false, OPT_HELP);
-    parser.addOption('v', "version", "Prints version information", false, OPT_VERSION);
-    parser.addOption('t', "threads", "Specifies number of threads to use (default: number of cores minus one)", true, OPT_THREADS);
-    parser.addOption('r', "restart", "Ignores saved render checkpoints and starts fresh from 0 spp", false, OPT_RESTART);
-    parser.addOption('c', "checkpoint", "Specifies render time in minutes before saving a checkpoint. A value of 0 disables checkpoints. Overrides the setting in the scene file", true, OPT_CHECKPOINTS);
-    parser.addOption('o', "output-directory", "Specifies the output directory. Overrides the setting in the scene file", true, OPT_OUTPUT_DIRECTORY);
+
+    StandaloneRenderer renderer(parser, std::cout);
 
     parser.parse(argc, argv);
 
-    if (argc < 2 || parser.isPresent(OPT_HELP)) {
-        parser.printHelpText();
-        return 0;
-    }
     if (parser.isPresent(OPT_VERSION)) {
         std::cout << "tungsten, version " << VERSION_STRING << std::endl;
-        return 0;
+        std::exit(0);
     }
 
-    if (parser.isPresent(OPT_THREADS)) {
-        int newThreadCount = std::atoi(parser.param(OPT_THREADS).c_str());
-        if (newThreadCount > 0)
-            threadCount = newThreadCount;
-    }
-    if (parser.isPresent(OPT_CHECKPOINTS))
-        checkpointInterval = std::atoi(parser.param(OPT_CHECKPOINTS).c_str());
+    renderer.setup();
 
-    embree::rtcInit();
-    //embree::rtcSetVerbose(1);
-    embree::rtcStartThreads(threadCount);
-
-    ThreadUtils::startThreads(threadCount);
-
-    Path outputDirectory;
-    if (parser.isPresent(OPT_OUTPUT_DIRECTORY)) {
-        outputDirectory = Path(parser.param(OPT_OUTPUT_DIRECTORY));
-        outputDirectory.freezeWorkingDirectory();
-        outputDirectory = outputDirectory.absolute();
-    }
-
-    for (const std::string &sceneFile : parser.operands()) {
-        std::cout << tfm::format("Loading scene '%s'...", sceneFile) << std::endl;
-        std::unique_ptr<Scene> scene;
-        try {
-            scene.reset(Scene::load(Path(sceneFile)));
-            scene->loadResources();
-        } catch (std::runtime_error &e) {
-            std::cerr << tfm::format("Scene loader for file '%s' encountered an unrecoverable error: \n%s",
-                    sceneFile, e.what()) << std::endl;
-            continue;
-        }
-
-        try {
-            DirectoryChange context(scene->path().parent());
-
-            if (parser.isPresent(OPT_OUTPUT_DIRECTORY))
-                scene->rendererSettings().setOutputDirectory(outputDirectory);
-
-            int maxSpp = scene->rendererSettings().spp();
-            std::unique_ptr<TraceableScene> flattenedScene(scene->makeTraceable());
-            std::unique_ptr<Renderer> renderer(new Renderer(*flattenedScene));
-
-            if (!parser.isPresent(OPT_CHECKPOINTS))
-                checkpointInterval = scene->rendererSettings().checkpointInterval();
-
-            if (scene->rendererSettings().enableResumeRender() && !parser.isPresent(OPT_RESTART)) {
-                std::cout << "Trying to resume render from saved state... ";
-                if (renderer->resumeRender(*scene))
-                    std::cout << "Successful" << std::endl;
-                else
-                    std::cout << "Unsuccessful" << std::endl;
-            }
-
-            std::cout << "Starting render..." << std::endl;
-            Timer timer, checkpointTimer;
-            double totalElapsed = 0.0;
-            while (!renderer->done()) {
-                renderer->startRender([](){});
-                renderer->waitForCompletion();
-                std::cout << tfm::format("Completed %d/%d spp", renderer->currentSpp(), maxSpp) << std::endl;
-                checkpointTimer.stop();
-                if (checkpointInterval > 0 && checkpointTimer.elapsed() > checkpointInterval*60) {
-                    totalElapsed += checkpointTimer.elapsed();
-                    std::cout << tfm::format("Saving checkpoint after %s", formatTime(totalElapsed).c_str()) << std::endl;
-                    Timer ioTimer;
-                    checkpointTimer.start();
-                    renderer->saveCheckpoint();
-                    if (scene->rendererSettings().enableResumeRender())
-                        renderer->saveRenderResumeData(*scene);
-                    ioTimer.stop();
-                    std::cout << tfm::format("Saving checkpoint took %s", formatTime(ioTimer.elapsed())) << std::endl;
-                }
-            }
-            timer.stop();
-
-            std::cout << tfm::format("Finished render. Render time %s", formatTime(timer.elapsed()).c_str()) << std::endl;
-
-            renderer->saveOutputs();
-            if (scene->rendererSettings().enableResumeRender())
-                renderer->saveRenderResumeData(*scene);
-        } catch (std::runtime_error &e) {
-            std::cerr << tfm::format("Renderer for file '%s' encountered an unrecoverable error: \n%s",
-                    sceneFile, e.what()) << std::endl;
-        }
-    }
+    while (renderer.renderScene());
 
     return 0;
 }
-
