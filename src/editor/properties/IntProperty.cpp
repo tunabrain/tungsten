6c3aaa52792006bdfaf81ab589b0e29346905919
Benedikt Bitterli <mail@noobody.org>
2015-03-10 22:59:38 +0100

    Added IntProperty to editor

---
+++ src/editor/properties/IntProperty.cpp
@@ -0,0 +1,40 @@
+#include "IntProperty.hpp"
+#include "PropertySheet.hpp"
+
+#include <QtGui>
+
+namespace Tungsten {
+
+IntProperty::IntProperty(QWidget *parent, PropertySheet &sheet, std::string name, int value, int min, int max,
+        std::function<bool(int)> setter)
+: _value(value),
+  _setter(std::move(setter))
+{
+    _nameLabel = new QLabel(QString::fromStdString(name + ":"), parent);
+    _spinner = new QSpinBox(parent);
+    _spinner->setMinimum(min);
+    _spinner->setMaximum(max);
+    _spinner->setValue(value);
+    connect(_spinner, SIGNAL(valueChanged(int)), this, SLOT(setValue(int)));
+
+    sheet.addWidget(_nameLabel, sheet.rowCount(), 0);
+    sheet.addWidget(_spinner, sheet.rowCount() - 1, 1);
+}
+
+void IntProperty::setValue(int newValue)
+{
+    if (newValue != _value) {
+        if (!_setter(newValue))
+            _spinner->setValue(_value);
+        else
+            _value = newValue;
+    }
+}
+
+void IntProperty::setVisible(bool visible)
+{
+    _nameLabel->setVisible(visible);
+    _spinner->setVisible(visible);
+}
+
+}
---
+++ src/editor/properties/IntProperty.hpp
@@ -0,0 +1,37 @@
+#ifndef INTPROPERTY_HPP_
+#define INTPROPERTY_HPP_
+
+#include "Property.hpp"
+
+#include <functional>
+
+class QSpinBox;
+class QLabel;
+
+namespace Tungsten {
+
+class PropertySheet;
+
+class IntProperty : public Property
+{
+    Q_OBJECT
+
+    int _value;
+    std::function<bool(int)> _setter;
+
+    QLabel *_nameLabel;
+    QSpinBox *_spinner;
+
+private slots:
+    void setValue(int newValue);
+
+public:
+    IntProperty(QWidget *parent, PropertySheet &sheet, std::string name, int value, int min, int max,
+            std::function<bool(int)> setter);
+
+    virtual void setVisible(bool visible) override;
+};
+
+}
+
+#endif /* INTPROPERTY_HPP_ */
