f9a78359888049d794a61ee72b9a3a8a277092c3
Benedikt Bitterli <mail@noobody.org>
2015-06-28 21:43:59 +0200

    Added functions for importance sampling a directionally projected box

--- src/core/sampling/SampleWarp.hpp
+++ src/core/sampling/SampleWarp.hpp
@@ -4,6 +4,7 @@
 #include "math/MathUtil.hpp"
 #include "math/Angle.hpp"
 #include "math/Vec.hpp"
+#include "math/Box.hpp"
 
 #include <algorithm>
 #include <cmath>
@@ -139,6 +140,43 @@ static inline float uniformTrianglePdf(const Vec3f &a, const Vec3f &b, const Vec
 static inline float powerHeuristic(float pdf0, float pdf1)
 {
     return (pdf0*pdf0)/(pdf0*pdf0 + pdf1*pdf1);
+}
+
+static inline Vec3f projectedBox(const Box3f &box, const Vec3f &direction, float faceXi, const Vec2f &xi)
+{
+    Vec3f diag = box.diagonal();
+    float areaX = diag.y()*diag.z()*std::abs(direction.x());
+    float areaY = diag.z()*diag.x()*std::abs(direction.y());
+    float areaZ = diag.x()*diag.y()*std::abs(direction.z());
+
+    float u = faceXi*(areaX + areaY + areaZ);
+
+    Vec3f result;
+    if (u < areaX) {
+        result.x() = direction.x() < 0.0f ? box.max().x() : box.min().x();
+        result.y() = box.min().y() + diag.y()*xi.x();
+        result.z() = box.min().z() + diag.z()*xi.y();
+    } else if (u < areaX + areaY) {
+        result.y() = direction.y() < 0.0f ? box.max().y() : box.min().y();
+        result.x() = box.min().x() + diag.x()*xi.x();
+        result.z() = box.min().z() + diag.z()*xi.y();
+    } else {
+        result.z() = direction.z() < 0.0f ? box.max().z() : box.min().z();
+        result.x() = box.min().x() + diag.x()*xi.x();
+        result.y() = box.min().y() + diag.y()*xi.y();
+    }
+
+    return result;
+}
+
+static inline float projectedBoxPdf(const Box3f &box, const Vec3f &direction)
+{
+    Vec3f diag = box.diagonal();
+    float areaX = diag.y()*diag.z()*std::abs(direction.x());
+    float areaY = diag.z()*diag.x()*std::abs(direction.y());
+    float areaZ = diag.x()*diag.y()*std::abs(direction.z());
+
+    return 1.0f/(areaX + areaY + areaZ);
 }
 
 }
