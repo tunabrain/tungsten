75cc502d10f50ec76501f8c62583781986ac9de8
Benedikt Bitterli <mail@noobody.org>
2015-06-29 14:06:21 +0200

    Implemented new camera sampling interface in thinlens camera

--- src/core/cameras/ThinlensCamera.cpp
+++ src/core/cameras/ThinlensCamera.cpp
@@ -74,7 +74,124 @@ rapidjson::Value ThinlensCamera::toJson(Allocator &allocator) const
     return std::move(v);
 }
 
-bool ThinlensCamera::generateSample(Vec2u pixel, PathSampleGenerator &sampler, Vec3f &throughput, Ray &ray) const
+bool ThinlensCamera::samplePosition(PathSampleGenerator &sampler, PositionSample &sample) const
+{
+    Vec2f lensUv = sampler.next2D(CameraSample);
+    Vec2f aperturePos = _aperture->sample(MAP_UNIFORM, lensUv);
+    aperturePos = (aperturePos*2.0f - 1.0f)*_apertureSize;
+
+    sample.p = _transform*Vec3f(aperturePos.x(), aperturePos.y(), 0.0f);
+    sample.weight = Vec3f(1.0f);
+    sample.pdf = _aperture->pdf(MAP_UNIFORM, lensUv)*sqr(0.5f/_apertureSize);
+    sample.Ng = _transform.fwd();
+
+    return true;
+}
+
+bool ThinlensCamera::sampleDirection(PathSampleGenerator &sampler, const PositionSample &point,
+        DirectionSample &sample) const
+{
+    Vec2u pixel(sampler.next2D(CameraSample)*Vec2f(_res));
+    return sampleDirection(sampler, point, pixel, sample);
+}
+
+bool ThinlensCamera::sampleDirection(PathSampleGenerator &sampler, const PositionSample &point, Vec2u pixel,
+        DirectionSample &sample) const
+{
+    float pdf;
+    Vec2f pixelUv = _filter.sample(sampler.next2D(CameraSample), pdf);
+    Vec3f planePos = Vec3f(
+        -1.0f  + (float(pixel.x()) + pixelUv.x())*2.0f*_pixelSize.x(),
+        _ratio - (float(pixel.y()) + pixelUv.y())*2.0f*_pixelSize.x(),
+        _planeDist
+    );
+    planePos *= _focusDist/planePos.z();
+
+    Vec3f lensPos = _invTransform*point.p;
+    Vec3f localD = (planePos - lensPos).normalized();
+
+    if (_catEye > 0.0f) {
+        Vec2f diaphragmPos = lensPos.xy() - _catEye*_planeDist*localD.xy()/localD.z();
+        if (diaphragmPos.lengthSq() > sqr(_apertureSize))
+            return false;
+    }
+
+    sample.d =  _transform.transformVector(localD);
+    sample.weight = Vec3f(1.0f);
+    sample.pdf = (_planeDist*2.0f)*(_planeDist*_ratio*2.0f)/cube(localD.z());
+
+    return true;
+}
+
+bool ThinlensCamera::sampleDirect(const Vec3f &p, PathSampleGenerator &sampler, LensSample &sample) const
+{
+    PositionSample point;
+    if (!samplePosition(sampler, point))
+        return false;
+
+    sample.d = point.p - p;
+    if (!evalDirection(sampler, point, DirectionSample(-sample.d), sample.weight, sample.pixel))
+        return false;
+
+    float rSq = sample.d.lengthSq();
+    sample.dist = std::sqrt(rSq);
+    sample.d /= sample.dist;
+    sample.weight *= point.weight/rSq;
+    return true;
+}
+
+bool ThinlensCamera::evalDirection(PathSampleGenerator &/*sampler*/, const PositionSample &point,
+        const DirectionSample &direction, Vec3f &weight, Vec2f &pixel) const
+{
+    Vec3f localLensPos = _invTransform*point.p;
+    Vec3f localDir = _invTransform.transformVector(direction.d);
+    if (localDir.z() <= 0.0f)
+        return false;
+
+    Vec3f planePos = localDir*(_focusDist/localDir.z()) + localLensPos;
+    planePos *= _planeDist/planePos.z();
+
+    if (_catEye > 0.0f) {
+        Vec2f diaphragmPos = localLensPos.xy() - _catEye*_planeDist*localDir.xy()/localDir.z();
+        if (diaphragmPos.lengthSq() > sqr(_apertureSize))
+            return false;
+    }
+
+    pixel.x() = (planePos.x() + 1.0f)/(2.0f*_pixelSize.x());
+    pixel.y() = (_ratio - planePos.y())/(2.0f*_pixelSize.x());
+    if (pixel.x() < -_filter.width() || pixel.y() < -_filter.width() ||
+        pixel.x() >= _res.x() || pixel.y() >= _res.y())
+        return false;
+
+    weight = Vec3f(sqr(_planeDist)/(4.0f*_pixelSize.x()*_pixelSize.x()*cube(localDir.z()/localDir.length())));
+    return true;
+}
+
+float ThinlensCamera::directionPdf(const PositionSample &point, const DirectionSample &direction) const
+{
+    Vec3f localLensPos = _invTransform*point.p;
+    Vec3f localDir = _invTransform.transformVector(direction.d);
+    if (localDir.z() <= 0.0f)
+        return false;
+
+    Vec3f planePos = localDir*(_focusDist/localDir.z()) + localLensPos;
+    planePos *= _planeDist/planePos.z();
+
+    if (_catEye > 0.0f) {
+        Vec2f diaphragmPos = localLensPos.xy() - _catEye*_planeDist*localDir.xy()/localDir.z();
+        if (diaphragmPos.lengthSq() > sqr(_apertureSize))
+            return false;
+    }
+
+    float u = (planePos.x() + 1.0f)*0.5f;
+    float v = (1.0f - planePos.y()/_ratio)*0.5f;
+    if (u < 0.0f || v < 0.0f || u > 1.0f || v > 1.0f)
+        return 0.0f;
+
+    return  (_planeDist*2.0f)*(_planeDist*_ratio*2.0f)/cube(localDir.z()/localDir.length());
+}
+
+bool ThinlensCamera::generateSample(Vec2u pixel, PathSampleGenerator &sampler, Vec3f &weight, Ray &ray) const
 {
     float filterPdf;
     Vec2f pixelUv = 0.5f + _filter.sample(sampler.next2D(CameraSample), filterPdf);
@@ -88,8 +205,8 @@ bool ThinlensCamera::generateSample(Vec2u pixel, PathSampleGenerator &sampler, V
     planePos *= _focusDist/planePos.z();
 
     Vec2f aperturePos = _aperture->sample(MAP_UNIFORM, lensUv);
-    throughput = Vec3f(1.0f);
     aperturePos = (aperturePos*2.0f - 1.0f)*_apertureSize;
+    weight = Vec3f(1.0f);
 
     Vec3f lensPos = Vec3f(aperturePos.x(), aperturePos.y(), 0.0f);
     Vec3f localDir = (planePos - lensPos).normalized();
--- src/core/cameras/ThinlensCamera.hpp
+++ src/core/cameras/ThinlensCamera.hpp
@@ -33,7 +33,16 @@ public:
     virtual void fromJson(const rapidjson::Value &v, const Scene &scene) override;
     virtual rapidjson::Value toJson(Allocator &allocator) const override;
 
-    virtual bool generateSample(Vec2u pixel, PathSampleGenerator &sampler, Vec3f &throughput, Ray &ray) const override;
+    virtual bool samplePosition(PathSampleGenerator &sampler, PositionSample &sample) const override final;
+    virtual bool sampleDirection(PathSampleGenerator &sampler, const PositionSample &point, DirectionSample &sample) const override;
+    virtual bool sampleDirection(PathSampleGenerator &sampler, const PositionSample &point, Vec2u pixel,
+            DirectionSample &sample) const override;
+    virtual bool sampleDirect(const Vec3f &p, PathSampleGenerator &sampler, LensSample &sample) const override;
+    virtual bool evalDirection(PathSampleGenerator &sampler, const PositionSample &point,
+            const DirectionSample &direction, Vec3f &weight, Vec2f &pixel) const override;
+    virtual float directionPdf(const PositionSample &point, const DirectionSample &direction) const override;
+
+    virtual bool generateSample(Vec2u pixel, PathSampleGenerator &sampler, Vec3f &weight, Ray &ray) const override;
 
     virtual bool isDirac() const override;
 
