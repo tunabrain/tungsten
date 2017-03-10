2255698733ee21fc214bc7ce77757c4fbda46374
Benedikt Bitterli <mail@noobody.org>
2017-03-10 11:06:33 -0500

    Implement untracked rng generation in WritablePathSampleGenerator

--- src/core/sampling/WritablePathSampleGenerator.hpp
+++ src/core/sampling/WritablePathSampleGenerator.hpp
@@ -15,6 +15,22 @@ public:
     virtual void putDiscrete(int numChoices, int choice) = 0;
     virtual void put1D(float value) = 0;
     virtual void put2D(Vec2f value) = 0;
+
+    virtual float untracked1D() = 0;
+    inline Vec2f untracked2D()
+    {
+        float a = untracked1D();
+        float b = untracked1D();
+        return Vec2f(a, b);
+    }
+    inline bool untrackedBoolean(float pTrue)
+    {
+        return untracked1D() < pTrue;
+    }
+    inline int untrackedDiscrete(int numChoices)
+    {
+        return int(untracked1D()*numChoices);
+    }
 };
 
 }
