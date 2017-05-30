9fd8a945ffa86c95d8cbf7466120f959a96772c4
Benedikt Bitterli <mail@noobody.org>
2017-05-30 15:30:12 -0400

    Implement Rayleigh phase function inversion

--- src/core/phasefunctions/RayleighPhaseFunction.cpp
+++ src/core/phasefunctions/RayleighPhaseFunction.cpp
@@ -1,6 +1,7 @@
 #include "RayleighPhaseFunction.hpp"
 
 #include "sampling/PathSampleGenerator.hpp"
+#include "sampling/SampleWarp.hpp"
 
 #include "math/TangentFrame.hpp"
 #include "math/MathUtil.hpp"
@@ -44,6 +45,21 @@ bool RayleighPhaseFunction::sample(PathSampleGenerator &sampler, const Vec3f &wi
     ));
     sample.weight = Vec3f(1.0f);
     sample.pdf = rayleigh(cosTheta);
+    return true;
+}
+
+bool RayleighPhaseFunction::invert(WritablePathSampleGenerator &sampler, const Vec3f &wi, const Vec3f &wo) const
+{
+    Vec3f w = TangentFrame(wi).toLocal(wo);
+    float cosTheta = w.z();
+    float u = 0.5f*(cosTheta + std::sqrt(4.0f + cosTheta*cosTheta));
+    float u3 = u*u*u;
+    float z = (u3*u3 - 1.0f)/(2.0f*u3);
+    float xi2 = (z + 2.0f)*0.25f;
+    float xi1 = SampleWarp::invertPhi(w, sampler.untracked1D());
+
+    sampler.put2D(Vec2f(xi1, xi2));
+
     return true;
 }
 
